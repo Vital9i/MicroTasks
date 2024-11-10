8a7cf3ad11e3b4c099c1da19ada9cd1e292fb3fd
Vital9i <vitalikdoiniak@gmail.com>
2024-11-10 15:00:23 +0300

    11

---
+++ src/components/NewComponent/NewComponent.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type NewComponentPropsType = {
+    students: Array<StydentType>
+}
+
+type StydentType = {
+    id: number
+    name: string
+    age: number
+}
+
+export const NewComponent = (props: NewComponentPropsType) => {
+
+    const topCars = [
+        { manufacturer: 'BMW', model: 'm5cs' },
+        { manufacturer: 'Mercedes', model: 'e63s' },
+        { manufacturer: 'Audi', model: 'rs6' }
+    ]
+
+
+    return (
+<table>
+    <tr>
+        <th>manufacturer</th>
+        <th>model</th>
+    </tr>
+    
+    {topCars.map((car,index)=>{
+        return(
+        <tr key={index}>
+            <td>{car.manufacturer}</td>
+            <td>{car.model}</td>
+        </tr>
+        )
+    })}
+    
+</table>
+    );
+};
+
