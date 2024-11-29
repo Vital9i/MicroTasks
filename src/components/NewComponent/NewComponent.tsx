b693da555552af9572fa9b3c82059a46917eef98
Vital9i <vitalikdoiniak@gmail.com>
2024-11-29 15:07:23 +0300

    Method Map 2.0

--- src/components/NewComponent/NewComponent.tsx
+++ src/components/NewComponent/NewComponent.tsx
@@ -1,41 +1,55 @@
 import React from 'react';
 
-type NewComponentPropsType = {
-    students: Array<StydentType>
+type NewComponentsPropsType = {
+    students: Array<StudentType>
 }
 
-type StydentType = {
+type StudentType = {
     id: number
     name: string
     age: number
 }
 
-export const NewComponent = (props: NewComponentPropsType) => {
 
+export const NewComponent = () => {
     const topCars = [
-        { manufacturer: 'BMW', model: 'm5cs' },
-        { manufacturer: 'Mercedes', model: 'e63s' },
-        { manufacturer: 'Audi', model: 'rs6' }
+        { manufacturer: 'BMW', model: 'X5' },
+        { manufacturer: 'Mersedes', model: 'MLS' },
+        { manufacturer: 'Audi', model: 'Q7' }
     ]
-
-
     return (
         <table>
             <tr>
-                <th>manufacturer:</th>
-                <th>model:</th>
+                {topCars.map((car, index) => {
+                    return (
+                        <th key={index}>{car.manufacturer}</th>
+                    )
+                })}
+
             </tr>
-
-            {topCars.map((car, index) => {
-                return (
-                    <tr key={index}>
-                        <td>{car.manufacturer}</td>
-                        <td>{car.model}</td>
-                    </tr>
-                )
-            })}
-
+            <tr>
+                {topCars.map((car, index) => {
+                    return(
+                    <td key={index}>{car.model}</td>
+                )})}
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
         </table>
     );
 };
 
+
+// <ul>
+//     {props.students.map((item) => {
+//         debugger
+//         return (
+//             <li key={item.id}>
+//                 <span>{item.name}</span>
+//                 <span> age: {item.age}</span>
+//             </li>
+//         )
+//     })}
+// </ul>
