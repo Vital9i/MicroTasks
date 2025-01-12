70582b5ba93987b767c44135ce8dce2725fbbe40
Vital9i <vitalikdoiniak@gmail.com>
2025-01-12 23:19:46 +0300

    Input Lesson 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,29 @@
 import React, { useState } from 'react';
-import { NewComponent } from './components/NewComponent/NewComponent';
 
-export type MoneyValue = "All" | "ruble" | "dollar"
-export const App = () => {
-    const [money, setMoney] = useState([
-        { banknote: "dollar", nominal: 100, number: "a123456789" },
-        { banknote: "dollar", nominal: 50, number: "b123456789" },
-        { banknote: "ruble", nominal: 100, number: "c123456789" },
-        { banknote: "dollar", nominal: 100, number: "d123456789" },
-        { banknote: "dollar", nominal: 50, number: "e123456789" },
-        { banknote: "ruble", nominal: 100, number: "f123456789" },
-        { banknote: "dollar", nominal: 50, number: "j123456789" },
-        { banknote: "ruble", nominal: 50, number: "h123456789" }
-    ])
 
-    const [filter, setFilter] = useState<MoneyValue>('All');
-
-    let currentMoney = money
-
-    const onClickFilterHandler = (FilterName: MoneyValue) => {
-        setFilter(FilterName)
-    }
-
-    if (filter === 'ruble') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'ruble')
-    }
-    if (filter === 'dollar') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'dollar')
-    }
-
+function App() {
+    const [message, setMessage] = useState([
+        { message: 'message1' },
+        { message: 'message2' },
+        { message: 'message3' },
+        { message: 'message4' },
+        { message: 'message5' }
+    ]
+    )
     return (
-        <>
-            <NewComponent onClickFilterHandler={onClickFilterHandler}
-                currentMoney={currentMoney} />
-        </>
+        <div className="App">
+            <div>
+                <input />
+                <button>+</button>
+            </div>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
     );
-
 }
+
+
 
